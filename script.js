3ebd22b3da55f678c1825016d26db21ed901c920
Md:Kawsar Ahmed <knownaskawsar@gmail.com>
2023-09-08 02:14:44 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,138 @@
+$('.alert-k1').click(function () {
+  Swal.fire({
+
+    imageUrl: '@original/assets/finance.PNG',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1GNWuSyYWt6MEdhAFXzgSqCTKwAiO-h1y/view?usp=drive_link">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-k2').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/OOP.png',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-k3').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/data.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-k4').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/math.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/drive/folders/1SUsiNafqtLUAUpli8GP6XZJAcRxHMeEe?usp=sharing">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k5').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/physics.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+$('.alert-k6').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/ct2.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+$('.alert-k7').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/mid.png',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+$('.alert-k8').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/ct.gif',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
